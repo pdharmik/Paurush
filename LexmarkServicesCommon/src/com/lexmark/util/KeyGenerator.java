a09ce5382120de01bab0a53ccafbc5656ccc4351
Arko Sarkar <arkosarkar343@gmail.com>
2016-03-31 14:04:00 -0400

    Removed the lengthArgs variable.

--- LexmarkServicesCommon/src/com/lexmark/util/KeyGenerator.java
+++ LexmarkServicesCommon/src/com/lexmark/util/KeyGenerator.java
@@ -2,9 +2,8 @@ package com.lexmark.util;
 
 public class KeyGenerator {
 	public static String generateKey(String... args){
-		int lengthArgs=args.length;
-		StringBuilder sb=new StringBuilder(40*lengthArgs);
-		for(int i=0;i<lengthArgs;i++){
+		StringBuilder sb=new StringBuilder(40*args.length);
+		for(int i=0;i<args.length;i++){
 			if(args[i]!=null){
 				sb.append(args[i]);
 			}
